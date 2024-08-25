948fa970cabbec295d487d2ffbe3828d5d947a32
BorisMakhalyanov <boris_03rus@mail.ru>
2024-08-25 21:08:32 +0300

    Add files via upload

--- module_6_2.py
+++ module_6_2.py
@@ -2,11 +2,8 @@ class Vehicle:
 
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self):
-        self.__color = None
-
-    def __init(self, owner, model, engine_power, color):
-        self.__owner = owner
+    def __init__(self, owner, model, engine_power, color):
+        self.owner = owner
         self.__model = model
         self.__engine_power = engine_power
         self.__color = color
@@ -24,7 +21,7 @@ class Vehicle:
         print(self.get_model())
         print(self.get_horsepower())
         print(self.get_color())
-        print(f'Владелец: {self.__owner}')
+        print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
         if new_color.lower() in self.__COLOR_VARIANTS:
@@ -34,15 +31,14 @@ class Vehicle:
 
 
 class Sedan(Vehicle):
-
     __PASSENGERS_LIMIT = 5
 
-    def __init__(self, owner, model, color, engine_power, passengers_limit=__PASSENGERS_LIMIT):
-        super().__init__(owner, model, color, engine_power)
+    def __init__(self, owner, model, engine_power, color, passengers_limit=__PASSENGERS_LIMIT):
+        super().__init__(owner, model, engine_power, color)
         self.__passengers_limit = passengers_limit
 
 
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
 
 # Изначальные свойства
 vehicle1.print_info()
@@ -50,7 +46,7 @@ vehicle1.print_info()
 # Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
-vehicle1.__owner = 'Vasyok'
+vehicle1.owner = 'Vasyok'
 
 # Проверяем что поменялось
 vehicle1.print_info()
